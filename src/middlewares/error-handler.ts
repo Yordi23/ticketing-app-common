5654eda360323395543235a1110140e6be5fbad3
Yordi Ogando <yordiogando@gmail.com>
2021-05-09 23:25:42 -0400

    Added error logging

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -1,16 +1,25 @@
-import { NextFunction, Request, Response } from "express";
-import { CustomError } from "../errors/custom-error";
+import { NextFunction, Request, Response } from 'express';
+import { CustomError } from '../errors/custom-error';
 
-export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
-    if (err instanceof CustomError) {
-        return res.status(err.statusCode).send({
-            errors: err.serializeErrors()
-        });
-    }
+export const errorHandler = (
+	err: Error,
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => {
+	if (err instanceof CustomError) {
+		return res.status(err.statusCode).send({
+			errors: err.serializeErrors(),
+		});
+	}
 
-    res.status(400).send({
-        errors: [{
-            message: 'Something went wrong'
-        }]
-    });
+	console.error(err);
+
+	res.status(400).send({
+		errors: [
+			{
+				message: 'Something went wrong',
+			},
+		],
+	});
 };
